6958cf9bfaa27b3a570f90146a8fe524e71c5e4b
AgusCF <acardetti295@alumnos.iua.edu.ar>
2025-11-10 17:53:42 -0300

    fix

---
+++ src/main/java/ar/edu/iua/iw3/controllers/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package ar.edu.iua.iw3.controllers;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IW3 Orders API")
+                        .version("v1")
+                        .description("API para administrar órdenes de carga - puntos 1..5 del práctico")
+                        .contact(new Contact().name("Grupo IW3").email("devs@example.com")));
+    }
+}
